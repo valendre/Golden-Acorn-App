b59f7e02582e720fd0e2f9321a1090c078deedfc
valendre <valendre@gmail.com>
2020-05-28 11:37:07 +0200

    Button and Display functions are written, both of them is stateless.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
-const myfirstelement = <h1>Hello React!</h1>
+let counter = 0;
 
-ReactDOM.render(myfirstelement, document.getElementById('root'));
+function Button(props) {
+    return (
+        <button
+            className="square" onClick={function() { 
+                alert(counter); 
+                }}
+        >
+            {props.label}
+        </button>
+    );
+}
+
+function Display(props) {
+    return <p>{props.children}</p>
+}
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <main>
+                <Button label="Buy one" value={counter} />
+                <Display>
+                    {counter}
+                </Display>
+                <Button label="Eat one" value={counter}/>
+            </main>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
