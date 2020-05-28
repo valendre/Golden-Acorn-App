4f8607cf63c037803b22fddfc8ba58e3ed119fce
valendre <valendre@gmail.com>
2020-05-28 15:42:42 +0200

    A basic router was added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+  export default function App() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home page</Link>
+              </li>
+              <li>
+                <Link to="/simple/states/">With states</Link>
+              </li>
+            </ul>
+          </nav>
+  
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/simple/states/">
+              <SimpleGoldenAcornApp />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 
 function Button(props) {
     return (
@@ -16,6 +52,10 @@ Button.propTypes = {
 
 function Display(props) {
     return <p>{props.children}</p>
+}
+
+function HomePage(){
+    return <h1>Golden Accorn is life! 🌰</h1>
 }
 
 class SimpleGoldenAcornApp extends React.Component {
@@ -40,6 +80,7 @@ class SimpleGoldenAcornApp extends React.Component {
     render() {
         return (
             <main>
+            <h1>Golden Accorn application with states 🌰</h1>
                 <Button
                     label="Buy one"
                     onClick={() => this.handleClick(1)}
@@ -58,6 +99,6 @@ class SimpleGoldenAcornApp extends React.Component {
 }
 
 ReactDOM.render(
-    <SimpleGoldenAcornApp />,
+    <App />,
     document.getElementById('root')
 );
